0feaf480cd10b168413c8fb4c328b8d9e19cd1f5
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2025-12-12 18:56:29 -0500

    ✨ feat(header): update Header export to use specific Astro file

--- src/widgets/header/ui/index.ts
+++ src/widgets/header/ui/index.ts
@@ -1,1 +1,4 @@
+export { default as BrandLogo } from './BrandLogo.astro';
+export { default as DesktopNav } from './DesktopNav.astro';
+export { default as MobileNav } from './MobileNav.astro';
 export { default as Header } from './Header.astro';
