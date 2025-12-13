46534ae60c25ffcae3981321cf12591afc74bd30
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2025-12-12 21:36:38 -0500

    ✨ feat(i18n): add translations for title sections in English and Spanish

---
+++ src/shared/config/i18n/dictionaries/title-sections.ts
@@ -0,0 +1,14 @@
+import { Language } from '../languages';
+
+export const titleSectionsTranslations = {
+  [Language.ENGLISH]: {
+    'title.experience': 'Experience',
+    'title.projects': 'Projects',
+    'title.about-me': 'About me',
+  },
+  [Language.SPANISH]: {
+    'title.experience': 'Experiencia',
+    'title.projects': 'Proyectos',
+    'title.about-me': 'Sobre mí',
+  },
+} as const;
