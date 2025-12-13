80f9a195467a42ae0afb30963ce4fcac215c3cb3
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2025-12-12 23:20:26 -0500

    ✨ feat(tech-stack): add TechStack widget to display technologies used in the portfolio

--- src/shared/config/i18n/dictionaries/title-sections.ts
+++ src/shared/config/i18n/dictionaries/title-sections.ts
@@ -4,11 +4,13 @@ export const titleSectionsTranslations = {
 	[Language.ENGLISH]: {
 		'title.experience': 'Experience',
 		'title.projects': 'Projects',
+		'title.technologies': 'Technologies & Tools',
 		'title.about-me': 'About me',
 	},
 	[Language.SPANISH]: {
 		'title.experience': 'Experiencia',
 		'title.projects': 'Proyectos',
+		'title.technologies': 'Tecnologías & Herramientas',
 		'title.about-me': 'Sobre mí',
 	},
 } as const;
