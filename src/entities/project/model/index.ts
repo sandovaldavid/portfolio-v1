55617e75754adfa1cc75d95c2d401db645c27a21
David Sandoval <dev@sandovaldavid.com>
2026-07-23 11:05:23 -0500

    refactor(project): expose localized project model API

--- src/entities/project/model/index.ts
+++ src/entities/project/model/index.ts
@@ -1,2 +1,13 @@
-export { getProjectsData } from './data';
-export type { ProjectItem, CaseStudy } from './types';
+export { PROJECT_METADATA, PROJECT_TECHNOLOGIES, isProjectId } from './metadata';
+export type { ProjectId, ProjectTechnologyId } from './metadata';
+export { getProjectBySlug, getProjectsData } from './queries';
+export type {
+	CaseStudy,
+	CaseStudyArchitecture,
+	CaseStudyEvidence,
+	CaseStudySource,
+	ProjectContentData,
+	ProjectContentEntry,
+	ProjectItem,
+	ProjectList,
+} from './types';
